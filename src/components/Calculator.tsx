84614731f7f0a2aa3bbe72993cb8c6bc798f47f6
alphaorderly <ilov1112@gmail.com>
2025-06-11 00:02:50 +0900

    feat: 빌드 에러 수정

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { GameObject, GridPosition, ProbabilityCell, PlacedObject, PlacementMode } from '../types/calculator';
 import { GRID_WIDTH, GRID_HEIGHT } from '../consts/gameData';
 import { calculateProbabilities } from '../utils/probabilityCalculator';
